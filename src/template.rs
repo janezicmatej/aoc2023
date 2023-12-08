000001705885043af4261ed3828e415e51c4f67d
Matej Janežič <janezic.mj@gmail.com>
2023-12-08 08:00:44 +0100

    feat: add read_file_part

--- src/template.rs
+++ src/template.rs
@@ -54,3 +54,14 @@ pub fn read_file(folder: &str, day: u8) -> String {
     let f = fs::read_to_string(filepath);
     f.expect("could not open input file").trim().to_string()
 }
+
+#[must_use]
+pub fn read_file_part(folder: &str, day: u8, part: u8) -> String {
+    let cwd = env::current_dir().unwrap();
+    let filepath = cwd
+        .join("data")
+        .join(folder)
+        .join(format!("{day:02}-{part}.txt"));
+    let f = fs::read_to_string(filepath);
+    f.expect("could not open input file").trim().to_string()
+}
